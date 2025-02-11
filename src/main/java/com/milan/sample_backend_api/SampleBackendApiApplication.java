b6651d562eb5b5782f9d63e7f98300f49dd45e43
milanchudik <milanchudik@192.168.1.105>
2025-02-11 15:35:05 +0100

    innitial commit

---
+++ src/main/java/com/milan/sample_backend_api/SampleBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.milan.sample_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milan/sample_backend_api/SampleBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milan.sample_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
